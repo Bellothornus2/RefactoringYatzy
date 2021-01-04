98548d376a69840f9dd178f489eb8f35e058cc4a
bellothornus2 <divanov@cifpfbmoll.eu>
2021-01-04 15:35:10 +0100

    refactor(RefactoringYatzy): :art: we chenged the "ones" to "aces" to best match the game rules

--- src/yatzy.py
+++ src/yatzy.py
@@ -21,7 +21,7 @@ class Yatzy:
         return 0
     
     @staticmethod
-    def ones( d1,  d2,  d3,  d4,  d5):
+    def aces( d1,  d2,  d3,  d4,  d5):
         sum = 0
         if (d1 == 1):
             sum += 1
--- test/test_yatzy.py
+++ test/test_yatzy.py
@@ -18,11 +18,11 @@ def test_yatzy():
         assert 0 == Yatzy.yatzy([6,6,6,6,3])
 
 
-def test_ones():
-        assert Yatzy.ones(1,2,3,4,5) == 1
-        assert 2 == Yatzy.ones(1,2,1,4,5)
-        assert 0 == Yatzy.ones(6,2,2,4,5)
-        assert 4 == Yatzy.ones(1,2,1,1,1)
+def test_aces():
+        assert Yatzy.aces(1,2,3,4,5) == 1
+        assert 2 == Yatzy.aces(1,2,1,4,5)
+        assert 0 == Yatzy.aces(6,2,2,4,5)
+        assert 4 == Yatzy.aces(1,2,1,1,1)
 
 
 def test_twos():
