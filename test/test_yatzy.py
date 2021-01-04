9c98f5f6dc4d22529a740e0c69ccf3c188f74b50
bellothornus2 <divanov@cifpfbmoll.eu>
2021-01-04 17:53:27 +0100

    refactor(RefactoringYatzy): :fire: we removed func and test cases that do not apply to the game rules
    such score_pair and two_pairs func

--- test/test_yatzy.py
+++ test/test_yatzy.py
@@ -45,18 +45,6 @@ def test_sixes():
     assert 6 == Yatzy.sixes([4,4,6,5,5])
     assert 18 == Yatzy.sixes([6,5,6,6,5])
 
-# esto no existe
-def test_one_pair():
-    assert 6 == Yatzy.score_pair(3,4,3,5,6)
-    assert 10 == Yatzy.score_pair(5,3,3,3,5)
-    assert 12 == Yatzy.score_pair(5,3,6,6,5)
-
-# esto no existe
-def test_two_Pair():
-    assert 16 == Yatzy.two_pair(3,3,5,4,5)
-    assert 18 == Yatzy.two_pair(3,3,6,6,6)
-    assert 0 == Yatzy.two_pair(3,3,6,5,4)
-
 #this is not "upper case" count and add threes, but instead, if you have a "trio", count that trio, and the pair that stays outside
 def test_three_of_a_kind():
     assert 18 == Yatzy.three_of_a_kind([3,3,3,4,5])
